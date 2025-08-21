deac6c0df5f342062d79f6288f1b0a735c91fcde
butterfriez <radradio234@gmail.com>
2025-08-20 17:45:56 -0700

    rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,19 @@ import { ConfigModule } from '@nestjs/config';
 import { FirebaseProvider } from './firebase.provider';
 import { HttpModule } from '@nestjs/axios';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     HttpModule,
     ScheduleModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
   ],
   controllers: [CatalogController],
   providers: [MetadataService, CatalogService, FirebaseProvider],
